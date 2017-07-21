1868f471c8880366b298d1f0f32d4e718f4f605a
Waleed <Waleed633@icloud.com>
2017-07-21 17:44:21 +0500

    v23

--- main.js
+++ main.js
@@ -34,53 +34,54 @@ for(var j = 1; j <=5; j++){
 
 
 
-    var selected = [];
+    var box1 = [];
             
-    $( "a#add2" ).click(function() {
+    $( "a#add1" ).click(function() {
         
             
-        if(selected.length<5){
+        if(box1.length<5){
 
             $( this ).toggleClass( "background_color" );
             var data = $(this).data('index');
             
-            function findbydata(selected) {
-                return selected.data === data;
+            function findbydata(box1) {
+                return box1.data === data;
             }
             
-            var task = selected.find(findbydata);
+            var task = box1.find(findbydata);
             
 
             if(task){
                 
-                var index = selected.indexOf(task);
-                selected.splice(index,1);
+                var index = box1.indexOf(task);
+                box1.splice(index,1);
                 
             
             }
             else{
                 
-                selected.push({data});
+                box1.push({data});
     
             }
-            console.table(selected);
+            console.table(box1);
         } 
 
         else{
 
             var data = $(this).data('index');
 
-            function findbydata(selected) {
-                return selected.data === data;
+            function findbydata(box1) {
+                return box1.data === data;
             }
             
-            var task = selected.find(findbydata);
+            var task = box1.find(findbydata);
             
 
             if(task){
                 
-                var index = selected.indexOf(task);
-                selected.splice(index,1);
+                var index = box1.indexOf(task);
+                box1.splice(index,1);
+                $( this ).toggleClass( "background_color" );
                 
             
             }
@@ -88,5 +89,237 @@ for(var j = 1; j <=5; j++){
 
 
     });
+
+
+    var box2 = [];
+            
+    $( "a#add2" ).click(function() {
+        
+            
+        if(box2.length<5){
+
+            $( this ).toggleClass( "background_color" );
+            var data = $(this).data('index');
+            
+            function findbydata(box2) {
+                return box2.data === data;
+            }
+            
+            var task = box2.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box2.indexOf(task);
+                box2.splice(index,1);
+                
+            
+            }
+            else{
+                
+                box2.push({data});
+    
+            }
+            console.table(box2);
+        } 
+
+        else{
+
+            var data = $(this).data('index');
+
+            function findbydata(box2) {
+                return box2.data === data;
+            }
+            
+            var task = box2.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box2.indexOf(task);
+                box2.splice(index,1);
+                $( this ).toggleClass( "background_color" );
+            
+            }
+        }     
+
+
+    });
+
+
+
+
+    var box3 = [];
+            
+    $( "a#add3" ).click(function() {
+        
+            
+        if(box3.length<5){
+
+            $( this ).toggleClass( "background_color" );
+            var data = $(this).data('index');
+            
+            function findbydata(box3) {
+                return box3.data === data;
+            }
+            
+            var task = box3.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box3.indexOf(task);
+                box3.splice(index,1);
+                
+            
+            }
+            else{
+                
+                box3.push({data});
+    
+            }
+            console.table(box3);
+        } 
+
+        else{
+
+            var data = $(this).data('index');
+
+            function findbydata(box3) {
+                return box3.data === data;
+            }
+            
+            var task = box3.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box3.indexOf(task);
+                box3.splice(index,1);
+                $( this ).toggleClass( "background_color" );
+                
+            
+            }
+        }     
+
+
+    });
+
+
+
+    var box4 = [];
+            
+    $( "a#add4" ).click(function() {
+        
+            
+        if(box4.length<5){
+
+            $( this ).toggleClass( "background_color" );
+            var data = $(this).data('index');
+            
+            function findbydata(box4) {
+                return box4.data === data;
+            }
+            
+            var task = box4.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box4.indexOf(task);
+                box4.splice(index,1);
+                
+            
+            }
+            else{
+                
+                box4.push({data});
+    
+            }
+            console.table(box4);
+        } 
+
+        else{
+
+            var data = $(this).data('index');
+
+            function findbydata(box4) {
+                return box4.data === data;
+            }
+            
+            var task = box4.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box4.indexOf(task);
+                box4.splice(index,1);
+                $( this ).toggleClass( "background_color" );
+                
+            
+            }
+        }     
+
+
+    });
+
+
+
+    var box5 = [];
+            
+    $( "a#add5" ).click(function() {
+        
+            
+        if(box5.length<5){
+
+            $( this ).toggleClass( "background_color" );
+            var data = $(this).data('index');
+            
+            function findbydata(box5) {
+                return box5.data === data;
+            }
+            
+            var task = box5.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box5.indexOf(task);
+                box5.splice(index,1);
+                
+            
+            }
+            else{
+                
+                box5.push({data});
+    
+            }
+            console.table(box5);
+        } 
+
+        else{
+
+            var data = $(this).data('index');
+
+            function findbydata(box5) {
+                return box5.data === data;
+            }
+            
+            var task = box5.find(findbydata);
+            
+
+            if(task){
+                
+                var index = box5.indexOf(task);
+                box5.splice(index,1);
+                $( this ).toggleClass( "background_color" );
+                
+            
+            }
+        }     
+
+
+    });
+
 
 });    
